1c9c05bd48c08bb240210170f24194ac5e8aad94
mojahidjouel <mojahidjouel95@gmail.com>
2023-12-17 13:27:50 +0600

    landshowcommit

--- resources/views/backend/land/show.blade.php
+++ resources/views/backend/land/show.blade.php
@@ -5,10 +5,23 @@
 <div class="row">
     <div class="col-12">
         <div class="card">
-            
-            
-        
-             
+            <div class="table-responsive">
+                <table class="table table-bordered mb-0">
+                    <thead>
+                        <tr>
+                            <th scope="col">{{__('#SL')}}</th>
+                            <th scope="col">{{__('Name')}}</th>
+                            <th scope="col">{{__('Mouza NO')}}</th>
+                            <th scope="col">{{__('RS NO')}}</th>
+                            <th scope="col">{{__('BS NO')}}</th>
+                            <th scope="col">{{__('Registration NO')}}</th>
+                            <th scope="col">{{__('Land Area')}}</th>
+                            <th scope="col">{{__('Acquire Date')}}</th>
+                             <th class="white-space-nowrap">{{__('Action') }}</th>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
         </div>
     </div>
 </div>
